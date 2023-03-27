e64d431bf8d78355b6bce4e7aa9dec03340b262d
wtdavis <wtdavis@wesleyan.edu>
2023-03-27 16:57:30 -0400

    killin it

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+namespace :api, defaults: {format: :json} do 
+  resources :pokemon, only: [:index, :create, do
+    resources :items, only: [:show]
+    resources :types, only: [:index]
+  end
+  resources :items, only: [:index, :edit, :destroy]
+resources :items
 
+end
   # Defines the root path route ("/")
   # root "articles#index"
 end
